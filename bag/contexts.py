978a3ceccb921999bdbf4e5b757e6336d46c95a8
anteCedens <jaksa.juric@yahoo.com.hr>
2021-01-24 16:30:00 +0000

    Add comments to contexts.py

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,4 +1,12 @@
-
+"""
+This function will return a dictionary (instead of a template, for instance)
+called 'context'.
+This is what's known as a 'context processor'.
+It's purpose is to make this dictionary available to all templates
+across the entire app.
+In order for this to work, we need to add it to the list of
+context processors in settings.py
+"""
 def bag_contents(request):
     
     context = {
