40bdd6f6add79c9dc4dd2d130afd1dacc2da1a45
anteCedens <jaksa.juric@yahoo.com.hr>
2021-01-24 23:36:20 +0000

    Set the 'session' bag variable

--- bag/contexts.py
+++ bag/contexts.py
@@ -22,6 +22,7 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
