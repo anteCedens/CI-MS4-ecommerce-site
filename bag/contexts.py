788100c7b4d7b629e413ed77dcb0314a5ac24605
anteCedens <jaksa.juric@yahoo.com.hr>
2021-01-24 23:39:16 +0000

    Add a comment about the session 'bag' variable

--- bag/contexts.py
+++ bag/contexts.py
@@ -22,6 +22,10 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    """
+    This is the bag variable stored in the 'session':
+    it contains all the items the user would like to purchase.
+    """
     bag = request.session.get('bag', {})
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
