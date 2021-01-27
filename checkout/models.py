e59c0a5f0e9f5c9a6e0f150a78d82bea074d2505
anteCedens <jaksa.juric@yahoo.com.hr>
2021-01-27 01:49:19 +0000

    Add original save method override, with comments

--- checkout/models.py
+++ checkout/models.py
@@ -43,6 +43,16 @@ def _generate_order_number(self):
     return uuid.uuid4().hex.upper()
 
 
+def save(self, *args, **kwargs):
+    """
+    Override the original save method to set the order number
+    if it hasn't been set already.
+    """
+    if not self.order_number:
+        self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+
 class OrderLineItem(models.Model):
 
     """ A line-item will be like an individual shopping bag item,
