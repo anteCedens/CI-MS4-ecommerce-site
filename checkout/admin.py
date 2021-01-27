248293668c5eb69c451a41d7d5dc75934fb6a8e4
anteCedens <jaksa.juric@yahoo.com.hr>
2021-01-27 11:07:12 +0000

    Add the list_display option to the OrderAdmin class, with comments

--- checkout/admin.py
+++ checkout/admin.py
@@ -10,9 +10,10 @@ class OrderAdmin(admin.ModelAdmin):
                        'grand_total',)
 
     """
-    The 'fields' option isn't absolutely necessary here, but it's 
+    The 'fields' option isn't absolutely necessary here, but it's
     here to allow us to specify the order of the fields in the admin interface,
-    which would otherwise be adjusted by Django due to the use of some read-only fields.
+    which would otherwise be adjusted by Django due to the use of some
+    read-only fields.
     This way the order of the fields stays the same as it appears in the model.
     """
     fields = ('order_number', 'date', 'full_name',
@@ -20,3 +21,11 @@ class OrderAdmin(admin.ModelAdmin):
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
               'order_total', 'grand_total',)
+
+    """
+    list_display option will restrict the columns that show up in
+    the order list to only a few key items.
+    """
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
