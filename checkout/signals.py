0e1aee4f8bbcd5a7bd94f5110f606548288ea12a
anteCedens <jaksa.juric@yahoo.com.hr>
2021-01-27 12:13:52 +0000

    Add update_on_save def

--- checkout/signals.py
+++ checkout/signals.py
@@ -6,3 +6,8 @@ is saved and after it's deleted respectively.
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import OrderLineItem
+
+
+def update_on_save(sender, instance, created, **kwargs):
+    # Update order total on lineitem update/create
+    instance.order.update_total()
