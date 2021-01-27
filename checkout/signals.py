1ccb42c100ee0b72c2b7c87378149e7066f72b2f
anteCedens <jaksa.juric@yahoo.com.hr>
2021-01-27 12:09:45 +0000

    Add a comment about the imports

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,3 +1,8 @@
+"""
+'Post', in this case, means after: so this implies these signals
+are sent by Django to the entire application after a model instance
+is saved and after it's deleted respectively.
+"""
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import OrderLineItem
