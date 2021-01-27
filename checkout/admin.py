72780922839ef88fbe0f8a0ca0e714dbee6a4e7c
anteCedens <jaksa.juric@yahoo.com.hr>
2021-01-27 02:33:43 +0000

    Set 'readonly_fields' and 'fields' for the OrderAdmin class, with comments

--- checkout/admin.py
+++ checkout/admin.py
@@ -2,3 +2,21 @@ from django.contrib import admin
 from .models import Order, OrderLineItem
 
 # Register your models here.
+
+
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+    """
+    The 'fields' option isn't absolutely necessary here, but it's 
+    here to allow us to specify the order of the fields in the admin interface,
+    which would otherwise be adjusted by Django due to the use of some read-only fields.
+    This way the order of the fields stays the same as it appears in the model.
+    """
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total',)
