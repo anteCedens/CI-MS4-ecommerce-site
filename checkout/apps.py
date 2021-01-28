e434e5ba698c19ffc70b54a33a1b04b5f083812b
anteCedens <jaksa.juric@yahoo.com.hr>
2021-01-28 03:09:10 +0000

    Add comments to checkout app's apps.py file

--- checkout/apps.py
+++ checkout/apps.py
@@ -5,4 +5,12 @@ class CheckoutConfig(AppConfig):
     name = 'checkout'
 
     def ready(self):
+        """
+        Overriding the ready method and importing our signals module
+        will let Django know that there's a new signals module with
+        some listeners in it.
+        So now every time a line item is saved or deleted, our custom
+        update total model method will be called, updating the order
+        totals automatically.
+        """
         import checkout.signals
