c31673400f7d76e42f1ce5f0faffd46dd22bf7c1
anteCedens <jaksa.juric@yahoo.com.hr>
2021-01-28 04:16:15 +0000

    Complete the checkout view
    - create an instance of our order form
    - create the template
    - create the context containing the order form
    - render it all out

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-
 from .forms import OrderForm
 
 
@@ -9,3 +8,11 @@ def checkout(request):
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
