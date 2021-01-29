95d9679b80e60d6f65d02998c117fca89c1eaa82
anteCedens <jaksa.juric@yahoo.com.hr>
2021-01-29 22:12:18 +0000

    Set stripe_public_key and stripe_secret_key variables within the 'checkout' def

--- checkout/views.py
+++ checkout/views.py
@@ -7,6 +7,8 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
