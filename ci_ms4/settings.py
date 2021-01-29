a7664fcfe09b4a7cf67fb21dca59fa91906c0756
anteCedens <jaksa.juric@yahoo.com.hr>
2021-01-29 21:52:52 +0000

    Change SECRET_KEY value from default to os.environ.get

--- ci_ms4/settings.py
+++ ci_ms4/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')i$q=x)hh0&r#$kbz$pz&0s2r+9f-dyxoh8c6!cp3@b#p9#@+3'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
