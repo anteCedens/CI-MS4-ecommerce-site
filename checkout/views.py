d663fb1dfdf3d27cd51c4dc74ae3a1955492b4bd
anteCedens <jaksa.juric@yahoo.com.hr>
2021-01-29 18:44:03 +0000

    Add stripe_public_key to the context in checkout app's views.py

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Hz79OBNKfgGBua74HhXEbsiuz3FVzNaqrGlv21i5s9nw9RMtnBdI7oUXxiwTr9dplWG3KSqbKlaLAnjDRg8dtUi00S99HD90a',
     }
 
     return render(request, template, context)
