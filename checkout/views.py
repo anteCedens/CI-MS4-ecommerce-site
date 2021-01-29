2b14031535dee262a5cb62cc0cc8e670b6b0f54b
anteCedens <jaksa.juric@yahoo.com.hr>
2021-01-29 22:15:40 +0000

    Set stripe.api.key and create PaymentIntent

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,7 @@ import stripe
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
@@ -19,6 +20,11 @@ def checkout(request):
     """ Multiply by a hundred and round it to zero decimal places,
     since stripe will require the amount to charge as an integer. """
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
