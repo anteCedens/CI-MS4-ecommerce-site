b89b70508bc25c9325228f2aa679f2ff888a90e2
anteCedens <jaksa.juric@yahoo.com.hr>
2021-01-30 01:36:13 +0000

    Add if-else clause to check is request.method POST

--- checkout/views.py
+++ checkout/views.py
@@ -10,21 +10,25 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.error(request, "There's nothing in your bag at the moment")
-        return redirect(reverse('products'))
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
 
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    """ Multiply by a hundred and round it to zero decimal places,
-    since stripe will require the amount to charge as an integer. """
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+    else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, "There's nothing in your bag at the moment")
+            return redirect(reverse('products'))
+
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        """ Multiply by a hundred and round it to zero decimal places,
+        since stripe will require the amount to charge as an integer. """
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
 
     order_form = OrderForm()
 
