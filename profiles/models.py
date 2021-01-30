c29cb57bc9a81efdfd7ce7307dae7d00f8e05648
anteCedens <jaksa.juric@yahoo.com.hr>
2021-01-30 12:05:19 +0000

    Add remainder of the fields to UserProfile, with comments

--- profiles/models.py
+++ profiles/models.py
@@ -12,3 +12,11 @@ class UserProfile(models.Model):
     # that each user can only have one profile,
     # and each profile can only be attached to one user.
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Here in the profile all these fields are optional, so null=True and blank=True
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country *', null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
