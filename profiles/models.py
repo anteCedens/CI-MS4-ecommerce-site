c19adbcfadd33f27ae9da3f708f7f75425c32680
anteCedens <jaksa.juric@yahoo.com.hr>
2021-01-30 11:55:53 +0000

    Create UserProfile class, with defined 'user', and comments

--- profiles/models.py
+++ profiles/models.py
@@ -1,2 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    """
+    A user profile model for maintaining default
+    delivery information and order history
+    """
+    # This is just like a foreign key, except that it specifies
+    # that each user can only have one profile,
+    # and each profile can only be attached to one user.
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
